7657b304607c57d58a9945015ab67240a23bed1a
OmarEssameldinMousa <omar.esameldin1111@gmail.com>
2025-07-08 17:51:29 +0300

    posting files endpoint

---
+++ src/models/enums/ResponseEnums.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class ResponseSignal(Enum):
+
+    FILE_VALIDATED_SUCCESS = "File_validated_successfully"
+    FILE_TYPE_NOT_SUPPORTED = "File_type_not_supported"
+    FILE_SIZE_EXCEEDED = "File_size_exceeded"
+    FILE_UPLOAD_FAILED = "File_upload_failed"
+    FILE_UPLOAD_SUCCESS = "File_upload_successful"
